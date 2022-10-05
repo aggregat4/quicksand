6e085ec842b8de8f113e3ebf7ce6ed99a7e78060
Boris Terzic <boris@terzic.be>
2022-10-05 14:58:28 +0200

    Test improvement

--- src/test/java/net/aggregat4/quicksand/ImapRetrievalTest.java
+++ src/test/java/net/aggregat4/quicksand/ImapRetrievalTest.java
@@ -1,12 +1,15 @@
 package net.aggregat4.quicksand;
 
 import com.sun.mail.imap.IMAPFolder;
+import com.sun.mail.imap.SortTerm;
 import jakarta.mail.Folder;
+import jakarta.mail.Message;
 import jakarta.mail.MessagingException;
 import jakarta.mail.Session;
 import jakarta.mail.Store;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
 
@@ -25,10 +28,15 @@ public class ImapRetrievalTest {
         System.out.printf("Name: %s%n", defaultFolder.getName());
         System.out.printf("Full Name: %s%n", defaultFolder.getFullName());
         System.out.printf("URL: %s%n", defaultFolder.getURLName());
-        if (defaultFolder instanceof IMAPFolder) {
-            System.out.printf("IMAP Folder Attributes: %s%n", List.of(((IMAPFolder) defaultFolder).getAttributes()));
+        if (defaultFolder instanceof IMAPFolder imapFolder) {
+            System.out.printf("IMAP Folder Attributes: %s%n", List.of(imapFolder.getAttributes()));
+            // NOTE: we need to open the folder in a certain mode
+            imapFolder.open(Folder.READ_ONLY);
+            Message[] sortedMessages = imapFolder.getSortedMessages(new SortTerm[]{SortTerm.DATE});
+            for (Message message: sortedMessages) {
+                System.out.printf("Message From: '%s', To: '%s', Subject: '%s' %n", Arrays.stream(message.getFrom()).toList(), Arrays.stream(message.getAllRecipients()).toList(), message.getSubject());
+            }
         }
-
         store.close();
     }
 }
