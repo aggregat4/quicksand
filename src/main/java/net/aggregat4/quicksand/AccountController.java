887aa34dcf5f8cb012414b0e45dc3976e40f8cbc
Boris Terzic <boris@terzic.be>
2022-10-08 16:38:27 +0200

    Aligns email pagination to the right Converts inline components to use macros rudimentary folder handling

--- src/main/java/net/aggregat4/quicksand/AccountController.java
+++ src/main/java/net/aggregat4/quicksand/AccountController.java
@@ -5,6 +5,7 @@ import net.aggregat4.quicksand.configuration.PebbleConfig;
 import net.aggregat4.quicksand.pebble.PebbleRenderer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
@@ -14,16 +15,32 @@ import java.util.Map;
 
 @RestController
 public class AccountController {
+    public static final int PAGE_SIZE = 100;
     private static final PebbleTemplate accountTemplate =
             PebbleConfig.getEngine().getTemplate("templates/account.peb");
 
-    @GetMapping(value = "/account/{id}", produces = {"text/html"})
-    public String accountPage(@PathVariable int id) throws IOException {
+    @GetMapping(value = "/accounts/{id}", produces = {"text/html"})
+    public String accountPage(@PathVariable int id, @RequestParam(required = false, defaultValue = "1") Integer from) throws IOException {
+        return accountFolderPage(id, 1, 1);
+    }
+
+    @GetMapping(value = "/accounts/{accountId}/folders/{folderId}", produces = {"text/html"})
+    public String accountFolderPage(@PathVariable int accountId, @PathVariable int folderId, @RequestParam(required = false, defaultValue = "1") Integer from) throws IOException {
+        int total = 2526;
+        if (from > total || from < 1) {
+            throw new IllegalArgumentException("Accounts page called with invalid pagination offset");
+        }
         Map<String, Object> context = new HashMap<>();
         context.put("bodyclass", "accountpage");
-        context.put("currentAccount", new Account(1, "foo@example.com"));
-        context.put("currentFolder", new Folder(1, "INBOX"));
-        context.put("pagination", new Pagination(1, 100, 2526, 100));
+        context.put("currentAccount", new Account(accountId, "foo@example.com"));
+        List<Folder> folders = List.of(new Folder(1, "INBOX"), new Folder(2, "Archive"), new Folder(3, "Sent"), new Folder(4, "Junk"));
+        Folder currentFolder = folders.stream()
+                .filter(f -> f.id() == folderId)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(String.format("Folderid %s is unknown", folderId)));
+        context.put("currentFolder", currentFolder);
+        context.put("folders", folders);
+        context.put("pagination", new Pagination(from, Math.min(from + PAGE_SIZE, total), total, PAGE_SIZE));
         context.put("accounts", List.of(new Account(1, "foo@example.com"), new Account(2, "bar@example.org")));
         return PebbleRenderer.renderTemplate(context, accountTemplate);
     }
--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -22,4 +22,30 @@ function init() {
             }
         })
     }
+    // open submenus on hover and close automatically after a timeout (instead of immediately disappearing with css hover)
+    const menuItems = document.querySelectorAll('li.has-submenu')
+    let menuTimer = null
+    Array.prototype.forEach.call(menuItems, function(el, i){
+        el.addEventListener("mouseover", function(event){
+            this.classList.add("open")
+            clearTimeout(menuTimer)
+        });
+        el.addEventListener("mouseout", function(event){
+            menuTimer = setTimeout(function(event){
+                document.querySelector(".has-submenu.open").className = "has-submenu"
+            }, 1000)
+        });
+        // activate the submenus on activation (click, keyboard activation, etc) for non sighted users
+        el.querySelector('a').addEventListener("click",  function(event){
+            if (!this.parentNode.classList.contains("open")) {
+                this.parentNode.classList.add("open")
+                this.setAttribute('aria-expanded', "true")
+            } else {
+                this.parentNode.classList.remove("open")
+                this.setAttribute('aria-expanded', "false")
+            }
+            event.preventDefault()
+            return false
+        });
+    });
 }
