90150757a27aa7ec3a48a87242f4abd5e7c85769
Boris Terzic <boris@terzic.be>
2022-10-08 16:33:30 +0200

    Added check to enable or disable caching based on development mode

--- src/main/java/net/aggregat4/quicksand/configuration/PebbleConfig.java
+++ src/main/java/net/aggregat4/quicksand/configuration/PebbleConfig.java
@@ -4,7 +4,11 @@ import com.mitchellbosecke.pebble.PebbleEngine;
 import net.aggregat4.quicksand.pebble.PebbleExtension;
 
 public class PebbleConfig {
-    private static final PebbleEngine engine = new PebbleEngine.Builder().extension(new PebbleExtension()).cacheActive(false).build();
+    private static final PebbleEngine engine =
+            new PebbleEngine.Builder()
+                    .extension(new PebbleExtension())
+                    .cacheActive(!Boolean.TRUE.equals(Boolean.parseBoolean(System.getProperty("devmode"))))
+                    .build();
 
     public static PebbleEngine getEngine() {
         return PebbleConfig.engine;
